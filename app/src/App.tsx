8267d28b40a08ba40bf61cf8690acaec17819c71
Gideon Wikina <widdy49@gmail.com>
2023-02-19 22:57:30 -0500

    buttons and functionality for clocking in and out

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import ClockIn from './ClockIn';
+import ClockOut from './ClockOut';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+          <h2> Minute Man </h2>
+        </header>
+        <div className="time-keeping">
+          <ClockIn />
+          <ClockOut />
+        </div>
     </div>
   );
 }
