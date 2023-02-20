c906abe98e232fabb7dbbd26c86266ec19d27e66
Frank Agnello <frank.agnello3.external@enel.com>
2023-02-20 11:30:48 -0500

    App auto-builds into API

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const path = __dirname + '../app/build/';
+const path = __dirname + '/build/';
 
 const app = express();
 
