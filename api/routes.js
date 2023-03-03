1924d50e5c67ffc5f0a49859d79ccc17016fbd68
Frank Agnello <frank.agnello3.external@enel.com>
2023-03-03 11:46:31 -0500

    API functional

--- api/routes.js
+++ api/routes.js
@@ -6,8 +6,9 @@ module.exports = app => {
   
     // Create a new Punch
     router.get("/", db.ping);
-    router.get("/user", db.getUser);
-    router.put("/in", db.clockIn);
-    router.put("/out", db.clockOut);
+    router.get("/user/:id", db.getUser);
+    router.put("/in/:id/:company", db.clockIn);
+    router.put("/out/:id/:company", db.clockOut);
+    router.get("/status/:id/:company", db.getStatus);
     app.use('/api/time', router);
   };
