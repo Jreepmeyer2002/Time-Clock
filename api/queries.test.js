5bf93f88099a9209daa27bccccad4449238984ed
Nathan Petrangelo <nathan@petrangelo.org>
2023-03-06 18:57:45 -0500

    Tested getUser query

--- api/queries.test.js
+++ api/queries.test.js
@@ -1,5 +1,29 @@
-const queries = require("queries")
+process.env.DEBUG = "true";
+const queries = require("./queries")
+const httpMocks = require('node-mocks-http');
 
-test('test query', () => {
-   expect(queries.getUser())
+describe("Test the queries", () => {
+   test('test ping', () => {
+      const response = httpMocks.createResponse();
+      queries.ping({}, response);
+      expect(response._getData()).toBe("<h1>TimeClock API</h1>");
+   });
+
+   test('test getUser', () => {
+      const request = httpMocks.createRequest({
+         method: 'GET',
+         params: {id: 1}
+      });
+      const response = httpMocks.createResponse();
+      queries.getUser(request, response);
+      var user = {
+         ID: 1,
+         fname: 'john',
+         lname: 'doe',
+         username: 'johndoe10',
+         companyID: '1',
+         password: 'password'
+      }
+      expect(response._getJSONData()).toStrictEqual(user);
+   });
 });
